b03fbe0e11d0ed064409843a935f431b37c32787
Joshua Choi <im.joshuac@gmail.com>
2026-08-04 08:18:07 -0700

    Initial Commit

---
+++ VideoExportAsImage/VideoExportAsImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoExportAsImageApp.swift
+//  VideoExportAsImage
+//
+//  Created by Joshua Choi on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct VideoExportAsImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
